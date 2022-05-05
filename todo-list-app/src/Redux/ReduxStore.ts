c21632025733806aa0a3f1007e08cb62045b282b
gogy9000 <goropashnyj9000@gmail.com>
2022-05-05 16:18:36 +0500

    reinstal nodmoduls

--- todo-list-app/src/Redux/ReduxStore.ts
+++
@@ -1,9 +0,0 @@
-import {combineReducers, createStore} from "redux";
-import {taskBlockReducer} from "./ToDoReducer";
-
-
-let rootReducer= combineReducers({
-    stateTaskBlock:taskBlockReducer
-})
-
-export let store= createStore(rootReducer)
