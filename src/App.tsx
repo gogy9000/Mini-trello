698e212aa4fb048e972e1228c2aac1962c3ce7b8
gogy9000 <goropashnyj9000@gmail.com>
2022-06-12 21:04:56 +0500

    fix double render App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import {TodoContainer} from "./ToDo/TodoContainer";
-import {Button, Container, Grid} from "@mui/material";
+import {Grid} from "@mui/material";
 import {PrimarySearchAppBar} from "./AppBar/AppBar";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import { thunks} from "./Redux/ToDoReducer";
-import {AppDispatchType, AppStateType} from "./Redux/ReduxStore";
+import {AppDispatchType} from "./Redux/ReduxStore";
 
 export const useAppDispatch = () => useDispatch<AppDispatchType>()
 
 export const App = React.memo( () =>{
-    console.log('!!')
+
         const dispatch = useAppDispatch()
-        const state=useSelector((state:AppStateType)=>state)
+
         useEffect(()=>{
             // @ts-ignore
             dispatch(thunks.getTodolistAndTasks())
         },[])
 
-        //
-        // const load = () => {
-        //     // @ts-ignore
-        //     dispatch(thunks.getTodolistAndTasks())
-        // }
 
         return (
 
             <>
-
                 <PrimarySearchAppBar/>
-                {/*<Button onClick={load}>load</Button>*/}
                 <Grid container direction='column' justifyContent='end' spacing={1} pl={3} pr={3}>
                     <TodoContainer/>
                 </Grid>
