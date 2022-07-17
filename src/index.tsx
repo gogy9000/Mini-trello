afcafc413edbb71b3b9a55391558e2cc4e399f9a
gogy9000 <goropashnyj9000@gmail.com>
2022-07-17 23:32:52 +0500

    yarn lock

--- src/index.tsx
+++ src/index.tsx
@@ -11,13 +11,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-          <App />
+          {!store.getState().appReducer.isInitialization?<div>load</div>:<App/>}
       </Provider>
         </BrowserRouter>
-   // </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
