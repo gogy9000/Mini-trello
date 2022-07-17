12502c8d80e62e62661257d262b045b6a41ac757
gogy9000 <goropashnyj9000@gmail.com>
2022-07-17 23:00:39 +0500

    yarn lock

--- src/App.tsx
+++ src/App.tsx
@@ -44,9 +44,10 @@ export const App = () => {
         return (
 
                     <>
-                        <PrimarySearchAppBar/>
-                        {stateApp.isWaitingApp && <LinearProgress/>}
-                        <TodoContainer/>
+                        <div>asd</div>
+                        {/*<PrimarySearchAppBar/>*/}
+                        {/*{stateApp.isWaitingApp && <LinearProgress/>}*/}
+                        {/*<TodoContainer/>*/}
                         {/*<Routes>*/}
                         {/*    <Route path='/' element={<TodoContainer/>}/>*/}
                         {/*    <Route path='/incubator-to-do-list' element={<TodoContainer/>}/>*/}
@@ -54,8 +55,8 @@ export const App = () => {
                         {/*    <Route path='/404' element={<h1>404:PAGE NOT FOUND</h1>}/>*/}
                         {/*    <Route path='*' element={<Navigate to='/404'/>}/>*/}
                         {/*</Routes>*/}
-                        <TransitionAlerts error={stateApp.networkError} clearErrorCallback={clearErrorCallback}/>
-                        <TransitionAlerts error={stateApp.clientsError[0]} clearErrorCallback={clearErrorCallback}/>
+                        {/*<TransitionAlerts error={stateApp.networkError} clearErrorCallback={clearErrorCallback}/>*/}
+                        {/*<TransitionAlerts error={stateApp.clientsError[0]} clearErrorCallback={clearErrorCallback}/>*/}
 
                     </>
 
