fce9a372fe8f942374206c3a38d80916391dd531
gogy9000 <goropashnyj9000@gmail.com>
2022-07-17 20:40:05 +0500

    yarn lock

--- src/Redux/ReduxStore.ts
+++ src/Redux/ReduxStore.ts
@@ -4,16 +4,16 @@ import {loadState, saveState} from "../local-storage-utils/Local-storage-utils";
 import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {composeWithDevTools} from "@redux-devtools/extension";
 import {actionsApp, appReducer, thunkApp} from "./AppReducer";
-import {actionsAuth, authReducer, thunkAuth} from "./auth/Auth";
+import {actionsAuth, authReducer} from "./auth/Auth";
 
 
 export type InferActionsType<T> = T extends { [keys: string]: (...args: any[]) => infer U } ? U : never
-export type InferThunksType<T> = T extends { [keys: string]: (...args: any[]) =>(dispatch:AppDispatchType,getState:AppRootStateType)=> infer U } ? Promise<U>: never
-export type UnionThunkType=InferThunksType<typeof thunkApp>
-export type UnionActionsType = InferActionsType<typeof actions | typeof actionsApp|typeof actionsAuth>
+export type InferThunksType<T> = T extends { [keys: string]: (...args: any[]) => (dispatch: AppDispatchType, getState: AppRootStateType) => infer U } ? Promise<U> : never
+export type UnionThunkType = InferThunksType<typeof thunkApp>
+export type UnionActionsType = InferActionsType<typeof actions | typeof actionsApp | typeof actionsAuth>
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppDispatchType = ThunkDispatch<AppRootStateType, unknown, UnionActionsType>
-export type AppThunk<ReturnType=any> = ThunkAction<ReturnType, AppRootStateType, unknown, UnionActionsType>
+export type AppThunk<ReturnType = any> = ThunkAction<ReturnType, AppRootStateType, unknown, UnionActionsType>
 
 const persistState = loadState()
 
@@ -30,10 +30,10 @@ let rootReducer = combineReducers({
 })
 
 
-export let store = legacy_createStore(rootReducer, persistState, composeEnhancers(applyMiddleware(thunk)))
+export let store = legacy_createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 store.subscribe(() => {
-    saveState(store.getState())
+    // saveState(store.getState())
 })
 
 // @ts-ignore
